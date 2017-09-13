c4bb1e6d67cecb5b952a1bb525cf25e25a278714
kevin <sutonlineskyon@gmail.com>
2017-09-13 14:39:03 +0800

    modify a little

--- src/main/java/cn/kevin/leetcode/easy/MaximumSubArray.java
+++ src/main/java/cn/kevin/leetcode/easy/MaximumSubArray.java
@@ -40,7 +40,7 @@ public class MaximumSubArray {
     }
 
     public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = {-2,-1,-3,-4,-1,-2,-1,-5,-4};
         System.out.println(maxSubArray(nums));
     }
 }
